abfb920fc9b6ad01d3b47e9b77fe7d2ebf48d320
Zee Mudia <iosazee1@gmail.com>
2023-01-21 15:55:48 +0000

    set up models, registered the models and added some data to the models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Booking(models.Model):
+    Name = models.CharField(max_length=255)
+    No_of_Guests = models.PositiveSmallIntegerField()
+    BookingDate = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.Name
+
+
+class Menu(models.Model):
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return self.Title

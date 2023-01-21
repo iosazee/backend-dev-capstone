03c6b5d623dfdd770c953c45b6b9cbc3e5b75c08
Zee Mudia <iosazee1@gmail.com>
2023-01-21 23:18:30 +0000

    set up API endpoints for menu and table crud operations

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.response import Response
+from .models import Booking, Menu
+from django.contrib.auth.models import User
+from .serializers import BookingSerializer, MenuSerializer, Userserializer
+from rest_framework import permissions
+
+
+
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = Userserializer
+    permission_classes = [permissions.IsAuthenticated]
+
+

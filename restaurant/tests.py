5dda2a34d2ee086775df7c47b4ee893a0b3aa572
Zee Mudia <iosazee1@gmail.com>
2023-01-24 12:56:56 +0000

    added unit tests for the models

--- restaurant/tests.py
+++ restaurant/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from .models import Booking, Menu
+from datetime import datetime
+from decimal import Decimal
 
-# Create your tests here.
+class BookingModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.booking = Booking.objects.create(
+            Name = 'Paul Murphy',
+            No_of_Guests = 6,
+        )
+
+    def test_fields(self):
+        self.assertIsInstance(self.booking.Name, str)
+        self.assertIsInstance(self.booking.No_of_Guests, int)
+
+    def test_timestamps(self):
+        self.assertIsInstance(self.booking.BookingDate, datetime)
+
+
+class MenuModelTest(TestCase):
+
+    def setUp(self):
+        Menu.objects.create(
+            Title = 'Vanilla IceCream',
+            Price = 6.99,
+            Inventory = 99
+        )
+
+    def test_fields(self):
+        vanilla = Menu.objects.get(Title='Vanilla IceCream')
+        self.assertEqual(vanilla.Price, Decimal('6.99'))
+        self.assertEqual(vanilla.Inventory, 99)
+
+
+
+# to do implement test for the views
